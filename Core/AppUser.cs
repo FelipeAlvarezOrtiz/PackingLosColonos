dcce4ba749557c97a62718cb2f14f81be203175a
Felipe Andres Ortiz Alvarez <me@felipealvarez.dev>
2024-12-29 20:16:52 -0300

    Agregar archivos de proyecto.

---
+++ Core/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace PackingLosColonos.Core
+{
+    public class AppUser : IdentityUser
+    {
+        [Required,MaxLength(130)]
+        public required string Nombre { get; set; }
+        public int Rut { get; set; }
+        [MaxLength(1)]
+        public required string Dv { get; set; }
+        public bool Activo { get; set; }
+    }
+}
